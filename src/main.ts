cb2e96ea5332cb93386f49336a2387aab4d3fb90
Nils <sabrinaundnils1@outlook.com>
2025-01-29 17:10:33 +0100

    Blog Datei Änderung

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { BlogComponent } from './blog/blog.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
+
+bootstrapApplication(BlogComponent, appConfig)
   .catch((err) => console.error(err));
