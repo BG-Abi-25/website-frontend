4327a0a24590a7420869726afed36141f80d66e9
Nils <contact@piste.dev>
2025-01-29 18:54:35 +0100

    fix something?

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-<<<<<<< HEAD
-=======
-import { AppComponent } from './app/app.component';
->>>>>>> 0cd24eadac5247a39d349bd2533a6e58cc6f43a1
 import { BlogComponent } from './app/blog/blog.component';
 
 bootstrapApplication(BlogComponent, appConfig)
