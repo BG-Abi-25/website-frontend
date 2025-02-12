4dfb3a1168792478cd9f69a67c5a381c412188b9
Bernd <berndkrohmer01@outlook.de>
2025-02-12 01:19:17 +0100

    feat: calendar hover

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -12,11 +12,14 @@ import { MatIconModule } from '@angular/material/icon';
   imports: [CommonModule, MatDatepickerModule, MatNativeDateModule, MatIconModule]
 })
 export class CalendarComponent implements OnInit {
-  events: { date: Date, title: string, description: string }[] = [
-    { date: new Date('2025-02-21'), title: 'Elternsprechtag', description: 'Eltern können sich mit den Lehrern austauschen.' },
-    { date: new Date('2025-03-10'), title: 'Pädagogischer Tag am BG', description: 'Fortbildungstag für das Lehrerkollegium.' },
-    { date: new Date('2025-04-01'), title: 'Französischer Abend am BG', description: 'Ein Abend mit französischer Kultur und Küche.' }
+  events: { date: Date, title: string, description: string, time: string }[] = [
+    { date: new Date('2025-02-21'), title: 'Elternsprechtag', description: 'Eltern können sich mit den Lehrern austauschen.', time: 'Ganztägig' },
+    { date: new Date('2025-03-10'), title: 'Pädagogischer Tag am BG', description: 'Fortbildungstag für das Lehrerkollegium.', time: 'Ganztägig' },
+    { date: new Date('2025-04-01'), title: 'Französischer Abend am BG', description: 'Ein Abend mit französischer Kultur und Küche.', time: '19:00 - 20:00 Uhr' }
   ];
+ 
+ 
+  
 
   constructor() {}
 
