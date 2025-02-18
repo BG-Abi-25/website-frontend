0b2be723688e89591b8f2efcc3a5db1e5fcdd4a7
Nico Hees <nicoo@hees-clan.de>
2025-02-18 21:47:11 +0100

    feat: hero begonnen

--- src/app/startpage/startpage.component.ts
+++ src/app/startpage/startpage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BlogsComponent } from "../blogs/blogs.component";
 import { CalendarComponent } from "../calendar/calendar.component";
+import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-startpage',
-  imports: [BlogsComponent, CalendarComponent],
+  imports: [BlogsComponent, CalendarComponent, HeroComponent],
   templateUrl: './startpage.component.html',
   styleUrl: './startpage.component.css'
 })
