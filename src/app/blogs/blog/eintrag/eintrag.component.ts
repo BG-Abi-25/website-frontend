dbcb450dc4a4d3bbfad046176065e73493fa6fbe
Nils.Näckel <nils.naeckel@icloud.com>
2025-02-25 10:13:40 +0100

    feat: author relative

--- src/app/blogs/blog/eintrag/eintrag.component.ts
+++ src/app/blogs/blog/eintrag/eintrag.component.ts
@@ -8,6 +8,6 @@ import { Component } from '@angular/core';
 })
 export class EintragComponent {
   text = "Das Burg-Gymnasium in Schorndorf stellt sich am 7. Februar mit einem breitgefächerten Angebot Grundschülern und Eltern vor. Besucher können sich von 15:00 bis ca. 18 Uhr einen Eindruck von der Schule machen. Schulleiter Marcus Vornhusen begrüßt die Gäste um 15 Uhr in der Karl-Wahl-Sporthalle (um pünktliches Erscheinen wird gebeten, da gleich zu Beginn die Einteilung in Führungsgruppen erfolgt) Anschließend werden Viertklässler und Eltern in getrennten Kleingruppen durch die Gebäude geführt. Verspätet Ankommende melden sich bitte in der Aula des BG. Schulleitung, Lehrer, Schülermitverwaltung, Schulsozialarbeiterin und Ganztagesbetreuung stehen für Gespräche zur Verfügung. Die Tiefgarage unter der Karl-Wahl-Sporthalle ist geöffnet. Für das leibliche Wohl ist gesorgt. Lernen Sie und Ihre Kinder das BG kennen! Sie sind herzlich willkommen! Wie funktioniert die Anmeldung Ihres Kindes am BG?"
-
+  author = "Michael Oelschlegel"
 }
 
