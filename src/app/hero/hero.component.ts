75d37e3d12598fc7a36da0984c7ea514a781011c
Nico <nicoo.hees@gmail.com>
2025-02-25 10:13:20 +0100

    feat: pfeile hinzugefügt

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,6 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
+  slides = [
+    'https://www.bjw.de/wp-content/uploads/2015/01/2826_01-1500x1000.jpg',
+    'https://bg.schorndorf.de/dateien/website/Fotos/2020-2021/BGSonnenuntergang.jpg',
+    'https://www.bjw.de/wp-content/uploads/2015/01/2826_19-1500x1000.jpg'
+  ];
+  currentSlide = 0;
 
+  ngOnInit(): void {
+    setInterval(() => {
+      this.changeSlide(1);
+    }, 5000);
+  }
+
+  changeSlide(direction: number): void {
+    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+  }
+
+  setSlide(index: number): void {
+    this.currentSlide = index;
+  }
 }
