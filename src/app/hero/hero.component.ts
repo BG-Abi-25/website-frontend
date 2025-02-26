1d2cba1bf30fb4be20f745f68b0bde0a9ad760e1
Nico <nicoo.hees@gmail.com>
2025-02-26 11:47:53 +0100

    fix: neuer versuch

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
@@ -6,25 +6,46 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css'
 })
-export class HeroComponent implements OnInit {
-  slides = [
+export class HeroComponent implements OnInit, OnDestroy {
+  images: string[] = [
     'https://www.bjw.de/wp-content/uploads/2015/01/2826_01-1500x1000.jpg',
     'https://bg.schorndorf.de/dateien/website/Fotos/2020-2021/BGSonnenuntergang.jpg',
     'https://www.bjw.de/wp-content/uploads/2015/01/2826_19-1500x1000.jpg'
   ];
-  currentSlide = 0;
+  currentIndex = 0;
+  progressBarWidth = 0;
+  interval: any;
+  progressInterval: any;
 
-  ngOnInit(): void {
-    setInterval(() => {
-      this.changeSlide(1);
-    }, 5000);
+  ngOnInit() {
+    this.startSlideshow();
   }
 
-  changeSlide(direction: number): void {
-    this.currentSlide = (this.currentSlide + direction + this.slides.length) % this.slides.length;
+  ngOnDestroy() {
+    clearInterval(this.interval);
+    clearInterval(this.progressInterval);
   }
 
-  setSlide(index: number): void {
-    this.currentSlide = index;
+  startSlideshow() {
+    this.interval = setInterval(() => {
+      this.nextImage();
+    }, 5000); // Wechselt alle 5 Sekunden
+
+    this.progressInterval = setInterval(() => {
+      this.progressBarWidth += 100 / 5; // Progressbar für 5 Sekunden
+      if (this.progressBarWidth >= 100) {
+        this.progressBarWidth = 0;
+      }
+    }, 1000); // Fortschrittsbalken wird jede Sekunde aktualisiert
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    this.progressBarWidth = 0; // Reset der Progressbar
+  }
+
+  prevImage() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+    this.progressBarWidth = 0; // Reset der Progressbar
   }
 }
